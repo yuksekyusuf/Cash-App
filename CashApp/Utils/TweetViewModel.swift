7d030159ad6a20ec297acb4e1ceb1ea1097084a7
yuksekyusuf <68251921+yuksekyusuf@users.noreply.github.com>
2021-12-22 22:48:17 -0500

    Add Unit Test

---
+++ CashApp/Utils/TweetViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TweetViewModel.swift
+//  Twitter Clone
+//
+//  Created by Ahmet Yusuf Yuksek on 12/22/21.
+//
+
+import Foundation
---
+++ CashAppTests/CashAppTests.swift
@@ -0,0 +1,15 @@
+//
+//  CashAppTests.swift
+//  CashAppTests
+//
+//  Created by Ahmet Yusuf Yuksek on 12/22/21.
+//
+
+import XCTest
+@testable import CashApp
+
+class CashAppTests: XCTestCase {
+
+
+
+}
